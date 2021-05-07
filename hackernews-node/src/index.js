f569413b728bc2b3da961e12f64d00ea08cef5d6
silvio-luiz <silviobrandao@ufpr.br>
2021-05-06 22:58:22 -0300

    update 06/05

---
+++ hackernews-node/src/index.js
@@ -0,0 +1,55 @@
+const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const { PubSub } = require('apollo-server')
+const Subscription = require('./resolvers/Subscription')
+
+const pubsub = new PubSub()
+
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link
+}
+
+// 3
+const fs = require('fs');
+const path = require('path');
+const prisma = new PrismaClient()
+const { getUserId } = require('./utils');
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+    prisma,
+    pubsub,
+    userId:
+    req && req.headers.authorization
+      ? getUserId(req)
+      : null
+     };
+   }
+});
+
+  
+
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
+ 
+
---
+++ hackernews-node/src/script.js
@@ -0,0 +1,24 @@
+// 1
+const { PrismaClient } = require('@prisma/client')
+
+// 2
+const prisma = new PrismaClient()
+
+//3
+async function main() {
+  const newLinks = await prisma.link.create({
+      data: {
+          description: 'Fullstack tutorial for GraphQL',
+          url: 'www.howtographql.com',
+      },
+})
+}
+//4
+main()
+  .catch(e => {
+    throw e
+  })
+  // 5
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
